a30e8c0c9dfe8b7276749ec34ef04f718ff55cca
Hamed <141471503+salmanSiadat@users.noreply.github.com>
2025-06-08 22:28:58 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('bladder-tracker').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
